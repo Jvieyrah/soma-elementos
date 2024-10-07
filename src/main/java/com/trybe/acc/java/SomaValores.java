6d4399c716c4341e1ee8a26cb7cd3532e6ca2ffe
João Carlos Vieira Filho <me.contrate.aero@gmail.com>
2024-10-06 22:26:41 -0300

    Add files via upload

---
+++ src/main/java/com/trybe/acc/java/SomaValores.java
@@ -0,0 +1,27 @@
+package com.trybe.acc.java;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SomaValores {
+	
+  public static void main(String[] args) {
+    // Ler a entrada de um número pelo console
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+    int[] myArray = new int [3]; 
+
+    // Laço de repetição 
+    for (int index = 0; index < 3; index++) {
+      System.out.println("Digite um número:");
+      myArray[index] = scan.nextInt();
+    }
+    
+    int soma = myArray[0] + myArray[2];
+
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    System.out.println("A soma é: " + soma);
+
+  }
+
+}
